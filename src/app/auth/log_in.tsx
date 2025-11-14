ab857617fbf03a1e70ab3bc7516bb8e8ea6e8f5a
zeepha <242908493+simtea1048-lang@users.noreply.github.com>
2025-11-14 18:58:49 +0900

    MakeuseState

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -2,11 +2,12 @@ import { JSX } from 'react'
 import {
     View, Text, TextInput, TouchableOpacity, StyleSheet
     } from 'react-native'
-
+import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/Button'
 
-import { Link, router } from 'expo-router'
+
 
 const handlePress = (): void => {
     //　ログイン
@@ -14,12 +15,30 @@ const handlePress = (): void => {
 }
 
 const LogIn = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input} value='Email address' />
-                <TextInput  style={styles.input} value='Password' />
+                <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={(text) => { setEmail(text) }}
+                autoCapitalize='none'
+                keyboardType='email-address'
+                placeholder='Email Address'
+                textContentType='emailAddress'
+                />
+                <TextInput
+                style={styles.input}
+                value='Password'
+                onChangeText={(text) => { setPassword(text) }}
+                autoCapitalize='none'
+                secureTextEntry
+                placeholder='Password'
+                textContentType='password'
+                />
                 <Button label='submit' onPress={handlePress} />
                 <View style={styles.footer}>
                 <Text style={styles.footerText}>Not registered?</Text>
--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,4 +1,4 @@
-import { JSX } from 'react'
+import { JSX, useState } from 'react'
 import {
     View, Text, TextInput, TouchableOpacity, StyleSheet
 } from 'react-native'
@@ -12,27 +12,45 @@ const handlePress = (): void => {
 }
 
 const SignUp = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Sign Up</Text>
-                <TextInput style={styles.input} value='Email address' />
-                <TextInput  style={styles.input} value='Password' />
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => { setEmail(text) }}
+                    autoCapitalize='none'
+                    keyboardType='email-address'
+                    placeholder='Email Address'
+                    textContentType='emailAddress'
+                />
+                <TextInput
+                    style={styles.input}
+                    value='Password'
+                    onChangeText={(text) => { setPassword(text) }}
+                    autoCapitalize='none'
+                    secureTextEntry
+                    placeholder='Password'
+                    textContentType='password'
+                />
                 <Button label='Submit' onPress={handlePress} />
                 <View style={styles.footer}>
-                <Text style={styles.footerText}>Already registered?</Text>
-                <Link href='/auth/log_in' asChild>
-                <TouchableOpacity>
-                <Text style={styles.footerLink}>Log in</Text>
-                </TouchableOpacity>
-                </Link>
+                    <Text style={styles.footerText}>Already registered?</Text>
+                    <Link href='/auth/log_in' asChild>
+                        <TouchableOpacity>
+                            <Text style={styles.footerLink}>Log in</Text>
+                        </TouchableOpacity>
+                    </Link>
+                </View>
             </View>
         </View>
-    </View>
     )
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F0F4F8'
